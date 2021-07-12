ddb247efdf3a132e5f69283b3637f8147f1d268d
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-07-12 18:17:23 +0000

    changed port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.send("This server is running. Hello World!")
 });
 
-
-app.listen(3000, () => {
+const port = Process.env.PORT || 3000 ;
+app.listen(port, () => {
   console.log('server started');
 });
